b8640195172059c09ce45da2408860f74fb1cc6c
halleylucena51 <94462227+halleylucena51@users.noreply.github.com>
2021-11-19 09:19:51 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Balta.ContentContext;
+
+// Teste para o Github
+namespace Balta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var course = new Course();
+
+            course.Level = ContentContext.Enums.EContentLevel.Beginner;
+            
+            foreach (var item in course.Modules)
+            {
+            
+        }
+
+            
+
+        }
+    }
+}
